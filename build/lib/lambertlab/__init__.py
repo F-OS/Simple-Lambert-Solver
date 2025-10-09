0b3b6d4e02c7804550b5f2cfe8f57a358ae84714
Controller92 <letsflyrc@gmail.com>
2025-10-09 09:40:40 -0600

    Update .gitignore to exclude artifacts, build files, and generated outputs

--- build/lib/lambertlab/__init__.py
+++
@@ -1,9 +0,0 @@
-"""LambertLab: Interplanetary transfer analysis."""
-
-from .cli.main import main
-from .core.spice_io import load_kernels, rv_helio_spice, rv_helio_spice_q
-from .core.lambert_io import lambert_leg, vinf_vec, c3
-from .flows.em_only import compute_em_c3_tof, screen_em_grid
-from .viz.porkchop import plot_porkchop
-
-__version__ = "0.1.0"
